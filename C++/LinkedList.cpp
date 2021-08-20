be7853bbf28a91baa68b38eef62ad903b8d69158
Riju Mukherjee <rmukh561@gmail.com>
2021-08-20 21:57:18 +0530

    ✨ Added counters and index of Linkedlist

--- C++/LinkedList.cpp
+++ C++/LinkedList.cpp
@@ -12,11 +12,28 @@ class LinkedList
 {
 private:
     struct node *head;
+    int count;
+    int counter()
+    {
+        struct node *ptr = head;
+        if (head == NULL)
+        {
+            return 0;
+        }
+        int i = 1;
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+            i++;
+        }
+        return i;
+    }
 
 public:
     LinkedList()
     {
         head = NULL;
+        count = 0;
     }
     void addToLinkedListAtEnd(int data)
     {
@@ -27,6 +44,7 @@ public:
         if (head == NULL)
         {
             head = temp;
+            count++;
             return;
         }
         while (ptr->next != NULL)
@@ -34,6 +52,7 @@ public:
             ptr = ptr->next;
         }
         ptr->next = temp;
+        count++;
     }
     void addToLinkedListAtFront(int data)
     {
@@ -42,10 +61,39 @@ public:
         if (head == NULL)
         {
             head = temp;
+            count++;
             return;
         }
         temp->next = head;
         head = temp;
+        count++;
+    }
+    void addToLinkedListInMiddle(int data, int index)
+    {
+        return;
+    }
+    int indexOfData(int data)
+    {
+        if (head == NULL)
+        {
+            return -1;
+        }
+        struct node *ptr = head;
+        int i = 0;
+        while (ptr->next != NULL)
+        {
+            if (ptr->data == data)
+            {
+                return i;
+            }
+            ptr = ptr->next;
+            i++;
+        }
+        return -1;
+    }
+    int getCount()
+    {
+        return count;
     }
     void printLinkedList()
     {
@@ -53,10 +101,11 @@ public:
         {
             cout << "Empty Linked List" << endl;
         }
-        while (head != NULL)
+        struct node *ptr = head;
+        while (ptr != NULL)
         {
-            cout << "data: " << head->data << endl;
-            head = head->next;
+            cout << "data: " << ptr->data << endl;
+            ptr = ptr->next;
         }
     }
 };
@@ -67,11 +116,14 @@ int main()
     for (int i = 1; i <= 10; i++)
     {
         ll.addToLinkedListAtEnd(i);
+        cout << "Count: " << ll.getCount() << endl;
     }
-    for (int i = 0; i >-5; i--)
+    for (int i = 0; i > -5; i--)
     {
         ll.addToLinkedListAtFront(i);
+        cout << "Count: " << ll.getCount() << endl;
     }
     ll.printLinkedList();
+    cout << "Index of 3: " << ll.indexOfData(3) << endl;
     return 0;
 }
