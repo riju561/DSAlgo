7eb1b89c1e9183a6f08e1fa001931f2050e65d02
Riju Mukherjee <rmukh561@gmail.com>
2021-08-30 20:43:20 +0530

    ✨ javascript

---
+++ Javascript/Questions/Reverse_an_array.js
@@ -0,0 +1,6 @@
+const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const arr2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(arr1)
+console.log(arr1.reverse())
+console.log(arr2)
+console.log(arr2.reverse())
