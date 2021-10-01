2561edbb1700b6404fcd48f9c04653df55ced1a1
nikos-alexandris <nikos-alexandris@protonmail.com>
2021-10-01 20:42:16 +0300

    refactoring

--- C/DataStructures/MaxHeap/max_heap.h
+++ C/DataStructures/MaxHeap/max_heap.h
@@ -9,12 +9,30 @@
  * items the heap takes,
  * change the MaxHeapValue typedef.
  *
+ * NOTE: The heap keeps items
+ * ordered, so it needs to know
+ * how to order them. If the items
+ * are basic like int or char, then
+ * pass NULL to the mah_new function.
+ * If the values are a custom struct,
+ * then you will need to provide a
+ * pointer to a function of type
+ * int custom_compare(MaxHeapValue l, MaxHeapValue r)
+ * where the return values must be:
+ * if l == r -> 0
+ * if l <  r -> < 0
+ * if l >  r -> > 0
+ * So if, for example, you want to keep a max heap
+ * of strings, you can set the MaxHeapValue typedef
+ * to const char *, and pass the strcmp function to
+ * mah_new like so: MaxHeap h = mah_new(strcmp).
+ *
  * Brief usage:
  * To construct a MaxHeap item do
  *
  * ~~~~~~~~~~~~~~
  * MaxHeap h;
- * h = mah_new();
+ * h = mah_new(NULL);
  * ~~~~~~~~~~~~~~
  *
  * And to free it do
@@ -29,6 +47,8 @@
 
 typedef int MaxHeapValue;
 
+typedef int (*MaxHeapCmpFn)(MaxHeapValue a, MaxHeapValue b);
+
 typedef struct MaxHeap *MaxHeap;
 
 /*
@@ -42,9 +62,13 @@ typedef struct MaxHeap *MaxHeap;
  * The returned valued must
  * be freed with mih_free when
  * no longed needed.
+ *
+ * Read the description note if
+ * you don't know what to pass as
+ * argument.
  */
 MaxHeap
-mah_new(void);
+mah_new(MaxHeapCmpFn cmp);
 
 /*
  * ~~~~~~~~~~~~~~
